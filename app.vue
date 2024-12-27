6430e068149341e03f69cb3230a9d91f0f883cd7
wikky02H <109713162+wikky02H@users.noreply.github.com>
2024-12-27 10:41:37 +0530

    project basic setup

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline text-red-300">
+      Hello world!
+    </h1>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss','@pinia/nuxt'],
+  css: ["~/assets/css/main.css"],
+  modules: ['@pinia/nuxt'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
